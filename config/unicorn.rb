007a37d196c25a68c4bdd5be6619edf7be3cb1cb
Dusty Doris <dusty@doris.name>
2011-07-20 09:09:04 -0400

    auto-create the correct tmp/log directories

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 require 'fileutils'
-FileUtils.mkdir_p("log")
-FileUtils.mkdir_p("tmp")
-
 worker_processes 4
 timeout 30
 
 @_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+FileUtils.mkdir_p("#{@_path}/tmp")
+FileUtils.mkdir_p("#{@_path}/log")
+
 
 listen "#{@_path}/tmp/unicorn.sock", :backlog => 1024
 pid    "#{@_path}/tmp/unicorn.pid"
